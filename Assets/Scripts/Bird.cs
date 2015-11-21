53aa394bf0f9fd400b0d2de0b84a079dec520ae6
Andy Korth <andy@howlingmoonsoftware.com>
2015-11-21 12:27:34 -0600

    Better bird flight

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bird : MonoBehaviour {
+
+	private Vector3 vel;
+	public float speed;
+
+	public float lift = 0.2f;
+	public float drag = 0.9f;
+	public float turnSpeedX = 1.0f;
+	public float turnSpeedY = 1.0f;
+	public float rotationReturnRate = 0.1f;
+
+	public float gravity = 1;
+
+	public float rollRate = 0.3f;
+	public float rollMax = 85.0f;
+	private float roll = 0;
+
+	public SphereCollider coconutCollider;
+
+	private Quaternion rotation = Quaternion.identity;
+
+	public Quaternion flattenedRotation {
+		get{
+			return Quaternion.Euler(new Vector3(0, rotation.eulerAngles.y, 0));
+		}
+	}
+
+	public Quaternion unrolledRotation {
+		get{
+			return Quaternion.Euler(new Vector3(rotation.eulerAngles.x, rotation.eulerAngles.y, 0));
+		}
+	}
+
+	public void Start(){
+		Physics.IgnoreCollision (this.GetComponent<SphereCollider> (), coconutCollider, true);
+	}
+
+	void Update () {
+	
+		Vector3 inputDir = new Vector3 (Input.GetAxisRaw ("Vertical") * turnSpeedY, Input.GetAxisRaw ("Horizontal") * turnSpeedX, 0);
+		Vector3 thrust = this.transform.forward * speed;
+
+		Debug.DrawLine (transform.position, transform.position + thrust, Color.red);
+
+		Vector3 liftVec = vel.magnitude * lift * this.transform.up;
+		vel = vel * drag + liftVec + thrust;
+
+		transform.position += (vel + Vector3.down * gravity) * Time.deltaTime;
+
+		roll = Mathf.Lerp(roll, -Input.GetAxisRaw ("Horizontal") * rollMax, rollRate);
+
+		Debug.DrawLine (transform.position, transform.position + vel, Color.blue);
+
+		Vector3 euler = rotation.eulerAngles;
+
+
+		rotation = unrolledRotation * Quaternion.Euler (inputDir * Time.deltaTime) * Quaternion.AngleAxis (roll, Vector3.forward);
+
+//		rotation *= Quaternion.Euler (inputDir * Time.deltaTime);
+//
+//		rotation = Quaternion.Euler(new Vector3(euler.x + inputDir.x * Time.deltaTime, rotation.eulerAngles.y +inputDir.z * Time.deltaTime, roll));
+
+
+		rotation = Util.ConstantSlerp (rotation, flattenedRotation, rotationReturnRate * Time.deltaTime);
+		transform.localRotation = rotation;
+
+	}
+}
---
+++ Assets/Scripts/BirdFollowCamera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdFollowCamera : MonoBehaviour
+{
+
+	public Bird bird;
+	public float followRate = 0.9f;
+	public float rotationRate = 0.8f;
+
+	public Vector3 followDistance;
+
+	public void LateUpdate ()
+	{
+		Transform b = bird.transform;
+		Vector3 target = b.position + bird.flattenedRotation * followDistance;
+		transform.position = Vector3.Lerp (transform.position, target, followRate * 60.0f * Time.deltaTime);
+
+		transform.rotation = Quaternion.Lerp (transform.rotation, bird.flattenedRotation, rotationRate * 60.0f * Time.deltaTime);
+
+	}
+
+
+}
