dba2d023e0007de5c24521779d4a16af6ab0dc55
Andy Korth <andy@howlingmoonsoftware.com>
2015-11-21 18:31:17 -0600

    Randomize rings and nests

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,10 +9,14 @@ public class GameManager : SingletonScript<GameManager> {
 
 	public GameObject gotACoconut;
 
+	public Transform nests;
+	public Transform rings;
+
 	public int points = 0;
 
 	public void Start(){
 		MusicManager.i.PlayGameMusic ();
+		UpdateNestRingCombo ();
 	}
 
 	public void GameOver(){
@@ -24,6 +28,21 @@ public class GameManager : SingletonScript<GameManager> {
 	{
 		points += 1;
 		GameUI.i.UpdateScore (points);
+
+		UpdateNestRingCombo ();
+	}
+
+	public void UpdateNestRingCombo(){
+		ActivateOneIn (nests);
+		ActivateOneIn (rings);
+	}
+
+	public void ActivateOneIn(Transform parent){
+		int target = Random.Range(0,parent.childCount);
+		for(int i= 0; i < parent.childCount; i++){
+			GameObject go = parent.GetChild(i).gameObject;
+			go.SetActive (target == i);
+		}
 	}
 
 	public void Update(){
