bf617d68a4029c375722fcb7ba19e4d09d216e45
Andy Korth <andy@howlingmoonsoftware.com>
2015-11-22 00:45:21 -0600

    Add enemies

---
+++ Assets/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim.SetFloat ("Speed", Random.value + 0.3f);
+		transform.rotation = Quaternion.AngleAxis (Random.value * 360f, Vector3.up) * Quaternion.Euler(90f, 0, 0);
+	}
+
+}
